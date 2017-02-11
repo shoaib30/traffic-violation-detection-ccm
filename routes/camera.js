302884b375409d7b528a4f35a1a7c3564620bf3c
shoaib30 <shoaib30@gmail.com>
2017-02-11 23:46:49 +0530

    camera APIs

---
+++ routes/camera.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../config.js').camera
+var request = require('request')
+/* GET users listing. */
+
+var cameraModuleUrl = config.base_url + ":" + config.port
+
+router.get('/', function(req, res, next) {
+  res.send('Camera Communication API');
+});
+
+//API to trigger camera
+router.get('/trigger-camera', function(req, res, next)  {
+    //TO-DO respond back with JSON
+    /*{
+        "module":"sensor",
+        "timestamp":1486836445
+    }
+    */
+    console.log("Trigger camera at: " + cameraModuleUrl + req.path)
+    request(cameraModuleUrl + req.path, function (error, response, body) {
+        if (error)  {
+            console.log("Error Communicating with Camera Module")
+            res.send("Error in Camera Module")
+        }else if (!error && response.statusCode == 200) {
+            /*when camera module responds back with the data
+            {
+                "module":"camera",
+                "timestamp":1486836445,
+                "file_name":"file_300795",
+                "file_extension":"jpeg"
+            }
+            */
+            var data = JSON.parse(body)
+            //TO-DO store data in database
+            console.log("Response from Camera Module")
+            res.send("Recieved Response")
+        }else{
+            console.log("Error: " + response.statusCode)
+            res.send("Error in Camera Module")
+        }
+    })
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Traffic Violation Detection',
+      subtitle: 'Central Controller Module'
+    });
 });
 
 module.exports = router;
