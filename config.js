e47288f3892571a25752d0d22f705ce7927333d0
shoaib30 <shoaib30@gmail.com>
2017-04-29 15:00:02 +0530

    added central server

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ var config = {};
 config.camera = {};
 config.central_controller = {}
 config.image_processor = {}
+config.central_server = {}
 //defining camera module variables
 config.camera.base_url = "http://localhost"
 config.camera.port = "3001"
@@ -19,4 +20,10 @@ config.central_controller.apis = {}
 config.image_processor.base_url = "http://localhost"
 config.image_processor.port = "3002"
 
+
+config.central_server.base_url = "http://localhost"
+config.central_server.port = "5000"
+config.central_server.reqCred = {}
+config.central_server.reqCred.username = 'traffic-node'
+config.central_server.reqCred.password = 'password'
 module.exports = config
