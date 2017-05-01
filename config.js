2e550b285d9323fff67a7ce312714b2b89f23b3a
shoaib30 <shoaib30@gmail.com>
2017-05-01 14:16:42 +0530

    addded endpoint

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ config.image_processor.base_url = "http://localhost"
 config.image_processor.port = "3002"
 
 
-config.central_server.base_url = "http://localhost"
+config.central_server.base_url = "https://traffic-violation-detection-cc.run.aws-usw02-pr.ice.predix.io"
 config.central_server.port = "5000"
 config.central_server.reqCred = {}
 config.central_server.reqCred.username = 'traffic-node'
